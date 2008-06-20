7f63f696bb06af45bdf6058e24ab8d37079a9b87
jurgenfd <jurgenfd@3f501794-ca4c-0410-8399-9fafd7c53d92>
2008-06-20 17:21:52 +0000

    Debugged automation of phases 3 and 4.

--- trunk/src/Wattos/Star/NMRStar/File31.java
+++ trunk/src/Wattos/Star/NMRStar/File31.java
@@ -2198,7 +2198,7 @@ public class File31 {
                 Integer logicalOperationInteger = null;
 
                 while ( starDCTreeRId < tTTree.sizeRows ) { // scan the whole tree tagtable.                    
-//                    General.showDebug( "Working on dc tree rid: " + starDCTreeRId);
+                    General.showDebug( "Working on dc tree rid: " + starDCTreeRId);
                     boolean atomFoundForAllInDC = true;
                     if ( dCNumb !=  varDCtreeConstraintsID[starDCTreeRId] ) { // start a new constraint; will be executed on first tree row.                                                
                         if ( dCNumb != (varDCtreeConstraintsID[starDCTreeRId]-1)) {
@@ -2243,8 +2243,8 @@ public class File31 {
                         return false;
                     }
                     dCNodeNumb++; // safe after test above
-                    //General.showDebug( "Working on dc node number: " + dCNodeNumb);
-                    //General.showDebug( "Working on dc node rid   : " + currentDCNodeId);
+                    General.showDebug( "Working on dc node number: " + dCNodeNumb);
+                    General.showDebug( "Working on dc node rid   : " + currentDCNodeId);
                     if ( logicalOperationString != varDCtreeLogicoperation[ starDCTreeRId ] ) { // fast equality op possible because of nr list.
                         logicalOperationString  = varDCtreeLogicoperation[ starDCTreeRId ];
                         logicalOperationInteger = (Integer) DistConstr.logicalOperationString2Int.get( logicalOperationString );
