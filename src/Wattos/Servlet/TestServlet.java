2b921f2dd1146075389eefb591e7c093e9406a37
jurgenfd <jurgenfd@3f501794-ca4c-0410-8399-9fafd7c53d92>
2008-11-11 12:50:38 +0000

    Added OSexec of a list of processes for iCing.

---
+++ src/Wattos/Servlet/TestServlet.java
@@ -0,0 +1,39 @@
+package Wattos.Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        java.io.PrintWriter out = resp.getWriter();
+        out.println("<P>debugging.");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
