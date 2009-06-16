4a727c27fab66077a1a49828f16e90d8cf98c891
jurgenfd <jurgenfd@3f501794-ca4c-0410-8399-9fafd7c53d92>
2009-06-16 12:37:49 +0000

    - Added XML peaks to the list of allowed program/type.

--- python/Wattos/Utils/localConstants.py
+++ python/Wattos/Utils/localConstants.py
@@ -1,26 +1,23 @@
 import os
 
-urlDB            = 'http://tang.bmrb.wisc.edu/servlet_data/viavia/mr_mysql_backup/'
+#urlDB            = 'http://tang.bmrb.wisc.edu/servlet_data/viavia/mr_mysql_backup/'
+urlDB            = 'http://nmr.cmbi.ru.nl/~jd/viavia/mr_mysql_backup/'
 urlLists         = 'http://tang.bmrb.wisc.edu/wattos/lists/'
 
 share_dir         = '/share'
 share2_dir        = '/share2'
-big_dir           = '/big'
+big_dir           = '/Users/jd/NRG'
 root_dir          = '/'
-#print os.name # My windows xp returns nt here. Linux reports posix.
-if os.name == 'nt': # Windows drives
-    share_dir         = 'S:\\'
-    share2_dir        = 'Y:\\'
-    big_dir           = 'M:\\'
-    root_dir          = 'C:\\'
-    
+
 base_dir         = os.path.join(share_dir,'jurgen','CloneWars','DOCR1000')
-linkDir          = os.path.join(big_dir,'jurgen','DOCR_big_tmp_','link')
-tmpDir           = os.path.join(root_dir,'tmp')
+#linkDir          = os.path.join(big_dir,'jurgen','DOCR_big_tmp_','link')
+tmpDir           = os.path.join(root_dir,'tmpWattos')
 
 results_dir      = os.path.join(base_dir,'Results')
 csh_script_dir   = os.path.join(base_dir,'scripts')
 
 DUMP_DIR         = os.path.join(root_dir,'www','servlet_data','viavia','mr_mysql_backup')
+
+starDir          = os.path.join(big_dir,'star')
 
 #print "Read Wattos.Utils.localConstants.py version 0.3"
