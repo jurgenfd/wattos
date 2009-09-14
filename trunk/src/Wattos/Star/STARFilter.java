bfec2a323fa558d7d8bbba6d8a55223132a0b674
jurgenfd <jurgenfd@3f501794-ca4c-0410-8399-9fafd7c53d92>
2009-09-14 13:19:07 +0000

    Fixing issue 225 with relaxation data.

--- trunk/src/Wattos/Star/STARFilter.java
+++ trunk/src/Wattos/Star/STARFilter.java
@@ -16,12 +16,12 @@ import java.net.URL;
  *Reads and writes after filtering the input.
  *Possible filters intended are reordering and removing tags, tagtables, and
  *saveframes.
- *The third argument may be a dot so it will not filter but just pass thru which 
+ *The third argument may be a dot so it will not filter but just pass thru which
  *is useful for reformatting.
  * @author jurgen
  */
 public class STARFilter {
-    
+
     public static String SAVEFRAME_RULE_ORDER                           = "rule_order";
     public static String TAGNAME_RULE                                   = "_Rule";
     public static String SAVEFRAME_CATEGORY                             = "_Saveframe_category";
@@ -30,13 +30,13 @@ public class STARFilter {
     public static String TAG_NAME_IDENTIFYING                           = "_Tag_name_identifying";
     public static String VALUE                                          = "_Value";
     public static String LOCATION                                       = "_Location";
-        
+
     StarNode db = null;
-    
-    public STARFilter() {        
+
+    public STARFilter() {
     }
-    
-    
+
+
     /**
      * This will be a list of commands to do
      */
@@ -47,7 +47,7 @@ public class STARFilter {
             Class thisClass = this.getClass();
             Method m = thisClass.getMethod( ruleTypeName, new Class[]{ SaveFrame.class });
             Object result = m.invoke( this, new Object[] {sFOneRule});
-            return ((Boolean) result).booleanValue();            
+            return ((Boolean) result).booleanValue();
         } catch ( NoSuchMethodException nsme ) {
             General.showError("No method with command name: " + ruleTypeName);
             General.showThrowable(nsme);
@@ -56,11 +56,11 @@ public class STARFilter {
             General.showError("Error invoking reflected command: " + ruleTypeName);
             General.showThrowable(e);
         }
-        return false;            
+        return false;
     }
 
-    
-    
+
+
     /**
      * STARFilter using the rules given in the starnode.
      */
@@ -72,23 +72,23 @@ public class STARFilter {
         }
         if ( tTRules.used.cardinality() == 0 ) {
             General.showWarning("Empty rule file found.");
-            return true;            
-        }        
+            return true;
+        }
         for ( int ruleIdx =tTRules.used.nextSetBit(0);ruleIdx>=0;ruleIdx=tTRules.used.nextSetBit(ruleIdx+1)) {
             String ruleFrameCodeName = tTRules.getValueString(ruleIdx,TAGNAME_RULE);
             SaveFrame sFOneRule = dbRules.getSaveFrameByName(ruleFrameCodeName,true);
             if ( sFOneRule == null ) {
                 General.showError("Failed to find rule SaveFrame: " + ruleFrameCodeName);
                 return false;
-            }                
+            }
             if ( ! invoke(sFOneRule)) {
                 General.showError("Failed to invoke rule from: "+ruleFrameCodeName);
                 return false;
             }
         }
-        return true;        
+        return true;
     }
-    
+
     /**
      * STARFilter using the given files.
      */
@@ -98,82 +98,83 @@ public class STARFilter {
             General.doErrorExit("Expected three arguments but got null");
         }
         if ( args.length != 3 ) {
-            General.doErrorExit("Expected three arguments but got: " + args.length + General.eol +
-                    ". They were: " + Strings.toString(args));            
+            General.showError("Expected three arguments but got: " + args.length + General.eol +
+                    ". They were: " + Strings.toString(args));
+            return false;
         }
-        StarFileReader sfr = new StarFileReader(new DBMS());        
+        StarFileReader sfr = new StarFileReader(new DBMS());
         if ( ! InOut.filesExist(new String[] {args[0], args[2] })) {
             General.showError("Input file or rules file does not exist.");
             return false;
         }
-        
-        // Parse rules    
+
+        // Parse rules
         boolean skipFiltering = false;
         if ( args[2].equalsIgnoreCase(".") ) {
             // skip filtering itself.
             skipFiltering = true;
-        } else {        
+        } else {
             URL urlRules = InOut.getUrlFileFromName(args[2]);
-            long start = System.currentTimeMillis();        
+            long start = System.currentTimeMillis();
             StarNode snRules = sfr.parse(urlRules);
             long taken = System.currentTimeMillis() - start;
-            General.showDebug("Parse rules  : " + taken + "(" + (taken/1000.0) + " sec)" );                
+            General.showDebug("Parse rules  : " + taken + "(" + (taken/1000.0) + " sec)" );
             if ( snRules == null ) {
                 General.showError("Failed to parse rules file.");
-                return false;            
+                return false;
             }
             dbRules = (DataBlock) snRules.get(0);
-        }        
-        
-        // Parse input 
-        sfr = new StarFileReader(new DBMS());     //to a roam in a new dbms   
+        }
+
+        // Parse input
+        sfr = new StarFileReader(new DBMS());     //to a roam in a new dbms
         URL url = InOut.getUrlFileFromName(args[0]);
-//        long start = System.currentTimeMillis();        
+//        long start = System.currentTimeMillis();
         StarNode sn = sfr.parse(url);
 //        long taken = System.currentTimeMillis() - start;
-//        General.showDebug("Parse took   : " + taken + "(" + (taken/1000.0) + " sec)" );                
+//        General.showDebug("Parse took   : " + taken + "(" + (taken/1000.0) + " sec)" );
         if ( sn == null ) {
             General.showError("Failed to parse input file.");
-            return false;            
+            return false;
         }
-        db = (DataBlock) sn.get(0);                
-        
-        
+        db = (DataBlock) sn.get(0);
+
+
         // STARFilter
         if ( ! skipFiltering ) {
-//            start = System.currentTimeMillis();        
+//            start = System.currentTimeMillis();
             if ( ! filter(dbRules)) {
                 General.showError("Failed to filter.");
                 return false;
             }
 //            taken = System.currentTimeMillis() - start;
-//            General.showDebug("Filter took  : " + taken + "(" + (taken/1000.0) + " sec)" );                
+//            General.showDebug("Filter took  : " + taken + "(" + (taken/1000.0) + " sec)" );
         }
-        
+
         // Unparse filtered results.
-//        start = System.currentTimeMillis();                
+//        start = System.currentTimeMillis();
         if ( ! db.toSTAR(args[1])) {
             General.showError("Failed to unparse to outputfile.");
-            return false;                        
+            return false;
         }
 //        taken = System.currentTimeMillis() - start;
-//        General.showDebug("Unparse took : " + taken + "(" + (taken/1000.0) + " sec)" );        
-        
+//        General.showDebug("Unparse took : " + taken + "(" + (taken/1000.0) + " sec)" );
+
         return true;
     }
-      
-    
-    
+
+
+
     /**E.g.
 save_insertTag_1
-   _Saveframe_category          insertTag   
+   _Saveframe_category          insertTag
    loop_
-      _Saveframe_category_name    
+      _Saveframe_category_name
       _Tag_name_identifying
       _Tag_name
       _Value
-      _Location # Use 0 or . to put the tag at the begin or end. 
-      
+      _Location # Use 0 or . to put the tag at the begin or end.
+
       distance_constraints  "_Dist_constraint.Auth_segment_code" "_Entry_id" ENTRY_ID 0
    stop_
 save_
@@ -192,8 +193,8 @@ save_
         String[] valueList      = tTSFCategoryName.getColumnString(VALUE);
         String[] locList        = tTSFCategoryName.getColumnString(LOCATION);
 
-        
-        if ( ! db.insertTag( sFCategoryList, 
+
+        if ( ! db.insertTag( sFCategoryList,
                              tagNameIdList,
                              tagNameList,
                              valueList,
@@ -203,16 +204,16 @@ save_
             General.showError("Failed to insertTag for tagNameList: " + Strings.toString(tagNameList));
             return false;
         }
-        return true;        
+        return true;
     }
-            
-            
+
+
     /**E.g.
 save_removeAllButTagNames_1
    _Saveframe_category          removeAllButTagNames
-   _Saveframe_category_name     *   
+   _Saveframe_category_name     *
    loop_
-      _Tag_name      
+      _Tag_name
        "_Dist_constraint.Auth_segment_code"
        "_Dist_constraint.Auth_seq_ID"
        "_Dist_constraint.Auth_comp_ID"
@@ -220,7 +221,7 @@ save_removeAllButTagNames_1
    stop_
 save_
      *The SF category can be a wildcard. Will remove all tags in given SF category except for the
-     *tags given. If SF category is a wildcard remove all tags even if the tags to keep don't 
+     *tags given. If SF category is a wildcard remove all tags even if the tags to keep don't
      *occur, effectively removing all data but the specified. Specify a SF category if you don't
      *want that behaviour.
      */
@@ -231,7 +232,7 @@ save_
             return false;
         }
         String SFCatToKeep      = tTSFCategoryName.getValueString(0,SAVEFRAME_CATEGORY_NAME);
-        
+
         TagTable tTTagNameList = sFRule.getTagTable(TAG_NAME,true);
         if ( tTTagNameList == null ) {
             General.showError("Failed to get TagTable with tag name list: " + TAG_NAME);
@@ -239,23 +240,23 @@ save_
         }
         StringSet sS = tTTagNameList.getColumnStringSet(TAG_NAME);
         StringArrayList TtTagNameListToKeep = new StringArrayList(sS.hm.keySet());
-        
+
         if ( ! db.removeAllButTagNames( SFCatToKeep, TtTagNameListToKeep )) {
             General.showError("Failed to removeAllButTagNames for categoriy to keep: " + SFCatToKeep +
                     " and tag names: " + Strings.toString(TtTagNameListToKeep));
             return false;
         }
-        return true;        
+        return true;
     }
-            
-/**    
+
+/**
  *E.g.
 save_removeAllButTagTableWithTagName_1
 
    _Saveframe_category          removeAllButTagTableWithTagName
    _Saveframe_category_name     *
    _Tag_name                    "_Dist_constraint.Auth_segment_code"
-     
+
 save_
  *SF can be a wild card.
  */
@@ -273,9 +274,9 @@ save_
                     " and TtTagNameToKeep to keep: " + TtTagNameToKeep);
             return false;
         }
-        return true;        
+        return true;
     }
-    
+
     public boolean removeAllButSaveframeCategories( SaveFrame sFRule ) {
         TagTable tTSFCategoryName = sFRule.getTagTable(SAVEFRAME_CATEGORY_NAME,true);
         if ( tTSFCategoryName == null ) {
@@ -288,9 +289,9 @@ save_
             General.showError("Failed to removeAllButSaveframeCategories for categories to keep: " + Strings.toString(SFCatToKeep));
             return false;
         }
-        return true;        
+        return true;
     }
-    
+
     public boolean removeSaveframeCategories( SaveFrame sFRule ) {
         TagTable tTSFCategoryName = sFRule.getTagTable(SAVEFRAME_CATEGORY_NAME,true);
         if ( tTSFCategoryName == null ) {
@@ -303,23 +304,24 @@ save_
             General.showError("Failed to removeSaveframeCategories for categories to keep: " + Strings.toString(SFCatToRemove));
             return false;
         }
-        return true;        
+        return true;
     }
-    
+
     public static void showUsage() {
-        General.showOutput("Usage: java Wattos.Star.STARFilter inputFile outputFile ruleFile");        
-        General.showOutput("    ruleFile may be a '.' to denote no rules to be done; just a pass thru.");        
+        General.showOutput("Usage: java Wattos.Star.STARFilter inputFile outputFile ruleFile");
+        General.showOutput("    ruleFile may be a '.' to denote no rules to be done; just a pass thru.");
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         STARFilter Sfilter = new STARFilter();
-        General.setVerbosityToDebug();
-        General.showDebug("test");
+//        General.setVerbosityToDebug();
+//        General.showDebug("test");
         if ( ! Sfilter.filter(args)) {
+            showUsage();
             General.doErrorExit("Failed to STARFilter"); // give a error status code exit.
-		}
-    }    
+        }
+    }
 }
